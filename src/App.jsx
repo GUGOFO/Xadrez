f2cd938fe9398fc2d22d4295fe37dbc18489e965
GUGOFO <gugofogomes@gmail.com>
2026-09-23 12:27:50 -0300

    USs com Requisitos Pai

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import bispoAzul from './assets/pecas/azul/bispo.png'
 
 function App() {
 
   return (
     <>
-      
+      <img src={bispoAzul} alt="Bispo Branco" />
     </>
   )
 }
